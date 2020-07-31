eb940ca918ecfb362615101dec9a946fcdd1501c
leoienkejr <leonardoienkejr@gmail.com>
2020-07-30 21:05:32 -0300

    Define request_weather_api()  - API requests working

--- core.py
+++ core.py
@@ -1,3 +1,7 @@
+import os
+import requests
+
+
 def sanitize_query(query):
     allowed_chars = 'qwertyuiopasdfghjklçzxcvbnm'
     allowed_chars += allowed_chars.upper()
@@ -11,6 +15,15 @@ def sanitize_query(query):
     sanitized_query = ''.join([char for char in query
                                if char in allowed_chars])
     return sanitized_query
+
+
+def request_weather_api(query):
+    api_key = os.environ.get('OPENWEATHER_API_KEY')
+    api_url = 'http://api.openweathermap.org/data/2.5/weather'
+    payload = {'q': query, 'appid': api_key}
+
+    r = requests.get(api_url, params=payload)
+    return r.json()
 
 
 if __name__ == '__main__':
